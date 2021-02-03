b1a72958974fca39d1fb4bd3754671ff2ba09d58
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2021-02-03 23:31:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    db: 'mongodb+srv://izikoswheels:danil2006avgust@cluster0.k3nwc.mongodb.net/coin?retryWrites=true&w=majority', /* MongoDB connection link */
+    secretKey: '78789b8078789b8078789b8047780a7f2c7787878789b8018612a123fbc7f1b7efcf132', /* VK Mini Apps service secret key */
+    user_id: 1, /* VK user id */
+    token: '1', /* VK access token */
+    key: '11' // VK Coin merchant key
+
+};
